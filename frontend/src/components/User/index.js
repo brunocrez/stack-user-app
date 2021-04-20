920bcdc37e53608c32c5d495e8cefcabab92efae
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-20 08:59:15 -0300

    implementing first methods for user form

--- frontend/src/components/User/index.js
+++ frontend/src/components/User/index.js
@@ -2,13 +2,45 @@ import React, { Component } from 'react';
 
 import Main from '../template/Main';
 
+import { BASE_URL } from '../../URIs';
+
 const headerProps = {
   icon: 'user',
   title: 'Usuários',
   subtitle: 'Cadastro de Usuários: Listar, Incluir, Alterar e Excluir'
 }
 
+const initialState = {
+  user: { name: '', email: '' },
+  list: []
+}
+
 export default class User extends Component {
+
+  state = { ...initialState };
+
+  clear() {
+    this.setState({ user: initialState.user });
+  }
+
+  save() {
+    const user = this.state.user;
+    const currentAction = user.id ? 'put' : 'post';
+    const url = user.id ? `${BASE_URL}/${user.id}` : BASE_URL;
+
+    axios[currentAction](url, user)
+      .then(res => {
+        const list = this.getListUpdated(res.data);
+        this.setState({ user: initialState.user, list });
+      })
+  }
+
+  getListUpdated(user) {
+    const list = this.state.filter(e => e.id !== user.id);
+    list.unshift(user);
+    return list;
+  }
+
   render() {
     return (
       <Main {...headerProps}>
