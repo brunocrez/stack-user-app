3ccf0299b735c3d3f6142f60560c2d1a19dafd3a
Bruno Rezende <brunoc.rez@gmail.com>
2021-04-21 10:16:24 -0300

    finishing create, update and delete methods

--- frontend/src/components/User/index.js
+++ frontend/src/components/User/index.js
@@ -17,8 +17,24 @@ const initialState = {
 }
 
 export default class User extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.clear = this.clear.bind(this);
+    this.save = this.save.bind(this);
+  }
 
   state = { ...initialState };
+
+  componentDidMount() {
+    this.refresh();
+  }
+
+  refresh() {
+    axios
+      .get(BASE_URL)
+      .then(res => this.setState({ list: res.data }));
+  }
 
   clear() {
     this.setState({ user: initialState.user });
@@ -31,21 +47,121 @@ export default class User extends Component {
 
     axios[currentAction](url, user)
       .then(res => {
-        const list = this.getListUpdated(res.data);
-        this.setState({ user: initialState.user, list });
+        this.setState({ user: initialState.user });
+        this.refresh();
       })
   }
 
-  getListUpdated(user) {
-    const list = this.state.filter(e => e.id !== user.id);
-    list.unshift(user);
-    return list;
+  updateField(e) {
+    const user = { ...this.state.user };
+    user[e.target.name] = e.target.value;
+    this.setState({ user });
+  }
+
+  loadUser(user) {
+    this.setState({ user });
+  }
+
+  removeUser(user) {
+    axios
+      .delete(`${BASE_URL}/${user.id}`)
+      .then(() => this.refresh())
+  }
+
+  renderForm() {
+    return (
+      <div className="form">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>Nome</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                value={this.state.user.name}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o Nome" />
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6">
+            <div className="form-group">
+              <label>E-mail</label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                value={this.state.user.email}
+                onChange={e => this.updateField(e)}
+                placeholder="Digite o E-mail" />
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row">
+          <div className="col-12 d-flex justify-content-end">
+            <button
+              className="btn btn-primary"
+              onClick={this.save}>
+              Salvar
+            </button>
+            <button
+              className="btn btn-secondary ml-2"
+              onClick={this.clear}>
+              Cancelar
+            </button>
+          </div>
+        </div>      
+      </div>
+    );
+  }
+
+  renderTable() {
+    return (
+      <table className="table mt-4">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderRows()}
+        </tbody>
+      </table>
+    );
+  }
+
+  renderRows() {
+    return this.state.list.map(el => (
+      <tr key={el.id}>
+        <td>{el.name}</td>
+        <td>{el.email}</td>
+        <td>
+          <button
+            className="btn btn-warning mr-1"
+            onClick={() => this.loadUser(el)}>
+            <i className="fa fa-pencil"></i>
+          </button>
+          <button
+            className="btn btn-danger ml-1"
+            onClick={() => this.removeUser(el)}>
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </td>
+      </tr>
+    ));
   }
 
   render() {
     return (
       <Main {...headerProps}>
-        Cadastro
+        {this.renderForm()}
+        {this.renderTable()}
       </Main>
     );
   }
